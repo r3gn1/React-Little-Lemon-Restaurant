fd057cfd4994d0e79947bcb1d35004766802c6d4
r3gn1 <79879011+r3gn1@users.noreply.github.com>
2024-07-24 23:35:11 -0500

    React-Little-Lemon-Restaurant v0.1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# React-Little-Lemon-Restaurant
+<h1 align="center">Little Lemon Restaurant With React</h1>
+<br />
+
+<h3 align="center"><a href="https://reactlittlelemonrestaurant.netlify.app/"><strong>« https://reactlittlelemonrestaurant.netlify.app/»</strong></a></h3>
+<p align="center"> 
+  Little Lemon Restaurant for all!
+</p>
+<br />
+
+## Clone and install React-Little-Lemon-Restaurant
+
+```shell
+$ git clone https://github.com/r3gn1/React-Little-Lemon-Restaurant.git
+$ cd React-Little-Lemon-Restaurant
+$ npm install
+```
+
+### Launch
+
+```
+npm start
+```
---
+++ src/components/Footer.js
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+export default function Footer() {
+  return (
+    <footer>
+      <menu>
+        <li className="img-footer">
+          <img
+            src={require("../assets/footer-logo.png")}
+            alt="Little Lemon logo"
+          ></img>
+        </li>
+        <li className="contact">
+          <h1 className="footer-header">Navigation</h1>
+          <br></br>
+          <ul className="footer-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <a
+              href={require("../assets/menu.webp")}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Menu
+            </a>
+            <Link to="/reservations">Reservations</Link>
+            <Link to="/order">Order</Link>
+            <Link to="/login">Login</Link>
+          </ul>
+        </li>
+
+        <li>
+          <h1 className="footer-header">Contact</h1>
+          <br></br>
+          <ul className="footer-links">
+            <li>2002 Happy Way,</li>
+            <li>Miami Florida</li>
+            <br></br>
+            <li>(629)-236-5546</li>
+            <br></br>
+            <a
+              href="mailto: info@littlelemon.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              info@littlelemon.com
+            </a>
+          </ul>
+        </li>
+        <li>
+          <h1 className="footer-header">Connect</h1>
+          <br></br>
+          <ul className="footer-links">
+            <a
+              href="https://www.facebook.com/littlelemonrestaurant/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://www.instagram.com/littlelemonrestaurant/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+          </ul>
+        </li>
+      </menu>
+    </footer>
+  );
+}
